5cdd768cdb2ae82265f02042ec3b8260b69fe148
MohamedBenIsmaiel <generalbuismaiel@gmail.com>
2019-12-28 16:31:24 +0200

    algolia search server

--- db_connect.js
+++ db_connect.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('survey','root','password',{
+const sequelize = new Sequelize('algolia','root','password',{
     host: "localhost",
     dialect: "mysql",
 });
--- route/route.js
+++ route/route.js
@@ -2,8 +2,10 @@ const router = require('express-promise-router')();
 const _      = require('lodash');
 
 const algoliasearch = require('algoliasearch');
+//here the algolia setting (appKEy,adminKEy) wil save in .env file in the future 
 const client = algoliasearch('H5MC6V0P03', 'e2f32ed79dcc249b86532f6ae9bcb95a');
 const index = client.initIndex('beehive');
+index.setSettings({searchableAttributes:['userName','email']});
 
 const User   = require('../models/user');
 
@@ -13,6 +15,13 @@ router.post('/user',async(req,res,next)=>{
     const result = await User.create({
         userName,
         email,
+    });
+    // when add student to the database it will added to algolia too
+    // there is another method called addObjects can take array of objects
+    index.addObject(result, function(err, content) {
+        if (err) {
+          console.error(err);
+        }
     });
     res.send({
         result,
@@ -44,29 +53,41 @@ router.get('/user/:id',async(req,res,next)=>{
 })
 
 router.delete('/user/:id',async(req,res,next)=>{
-    const result = await User.destroy({
+       let result = await User.destroy({
         where: {
             id:req.params.id
-        }
-    });
+        },
+      });
+   //   when delete record from the database it will delete from algolia too 
+     await  index.deleteBy({
+        numericFilters: [
+         [
+           `id = ${req.params.id}`,
+         ]
+      ]
+   });
     
-    res.send({
+     res.send({
         result,
     })
 })
 
 router.put('/user/:id',async(req,res,next)=>{
-    const result = await User.update({
+    let result = await User.update({
         userName : req.body.userName,
         email    : req.body.email
     },
     { 
         where: {
         id:req.params.id
-        }
+        },
     }
 );
-   index.partialUpdateObjects(result,(err,res)=>{
+  result = await User.findByPk(req.params.id);
+
+  // when update record from my database it will update in algolia too
+  //there is another method called partialUpdateObjects can take array of objects
+   index.partialUpdateObject(result,(err,res)=>{
        if(err) {
            console.log(err);
        }
@@ -77,15 +98,11 @@ router.put('/user/:id',async(req,res,next)=>{
 })
 
 router.get('/search',async(req,res,next)=>{
-    index.search({
-        query: req.query.search
-        },
-        (err, { hits } = {}) => {
-          if (err) console.log(err);
-      
-          console.log(hits);
-        }
-      );
+    //  here the search api 
+   index.search(req.query.search).then(reslt =>{
+     res.send({reslt,}) 
+   });
+    
 })
 
 
